ff18d010677529e7cfb1ed23088a7a64abd95cfc
paul lahana <paul.lahana@gmail.com>
2020-04-03 16:21:09 +0200

    moved stysheets in pack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,5 +15,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-import '../stylesheets/application'
+import './stylesheets/application'
 import 'bootstrap'
