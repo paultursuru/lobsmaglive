6ed595c01b10a21bf2a95ca4396c2e339828b9d7
paul lahana <paul.lahana@gmail.com>
2020-04-03 15:22:22 +0200

    adding mixcloud cards with lobservatoire feed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+    require 'json'
+    require 'open-uri'
 
+    cloudcasts = "https://api.mixcloud.com/LobservatoireLive/cloudcasts/"
+    cloudcasts_serialized = open(cloudcasts).read
+    @tracks = JSON.parse(cloudcasts_serialized)
   end
 end
